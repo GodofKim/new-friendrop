3ef454b9c0118742ff3f9c8a425a2f37f827463a
GodofKim <kjh.original@gmail.com>
2016-10-01 19:02:17 +0900

    Add : Logout
    로그아웃 기능 추가.

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import HomeContent from './HomeContent';
 import Letter from './Letter';
 import Contact from './Contact';
 import ProfileCard from './Profile/ProfileCard';
+import Logout from './Logout';
 
 export { Header, Authentication, HomeContent,
   Letter, Contact,
-  ProfileCard };
+  ProfileCard, Logout };
