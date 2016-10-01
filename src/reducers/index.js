927c053cc9d9bf111f5f42ca9150d3f4238a7552
GodofKim <kjh.original@gmail.com>
2016-10-01 23:09:31 +0900

    Working : connecting List to server
    아직 작업중 에러 많음 주의

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import authentication from './authentication';
 import profile from './profile';
+import drop from './drop';
 import list from './list';
 
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   authentication, profile, list,
+  drop
 });
